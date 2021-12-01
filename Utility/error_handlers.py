8e603bf4919df53b300ce6436f1d04f9ac504173
Oscar-The-Femboy-Leopard <74582007+Oscar-The-Femboy-Leopard@users.noreply.github.com>
2021-12-01 01:24:11 +0000

    Updated to send a report to my server!

--- Utility/error_handlers.py
+++ Utility/error_handlers.py
@@ -9,71 +9,81 @@ class error_handling(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
+
+        priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
+
+        msg = discord.Embed(title="ERROR!", color=discord.Color.red())
+        msg.add_field(name="Member:", value=f"Name: `{ctx.author.display_name}`\nID: `{ctx.author.id}`)", inline=False)
+        msg.add_field(name="Guild:", value=f"Name: `{ctx.guild}`\nID: `{ctx.guild.id}`", inline=False)
+        msg.add_field(name="Channel:", value=f"Name: `{ctx.channel}`\nID: `{ctx.channel.id}`", inline=False)
+
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.reply('You are missing the required argument of: `{}`'.format(error))
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
 
         if isinstance(error, commands.MissingPermissions):
             await ctx.reply("You do not have all the permissions required!")
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
 
         if isinstance(error, commands.MissingRole):
             await ctx.reply("You are missing a role to complete this.")
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
 
         if isinstance(error, commands.BotMissingPermissions):
             await ctx.reply('I cannot complete this as I am missing the {} permission.'.format(error))
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
 
         if isinstance(error, commands.BadArgument):
             await ctx.reply("Bad argument! {}".format(error))
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
 
         if isinstance(error, commands.CommandNotFound):
             await ctx.reply(f"Command not found! Please refer to `{PREFIX}help` to avoid this error in the future.")
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
 
         if isinstance(error, commands.TooManyArguments):
             await ctx.reply(f"Too many arguments! Please refer to `{PREFIX}help <command>` for more information ")
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
 
         if isinstance(error, commands.CommandOnCooldown):
             await ctx.reply("Command is on cooldown! Please retry after: `{}`").format(error)
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
 
         if isinstance(error, commands.ChannelNotReadable):
             await ctx.reply("I cannot read this channel. Please check my permissions.")
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
 
         if isinstance(error, commands.EmojiNotFound):
             await ctx.reply("Cannot find the required emoji.\n\n{}").format(error)
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
 
         if isinstance(error, discord.Forbidden):
             await ctx.reply("I am forbidden: {}").format(error)
-            priv = self.client.get_guild(488623700539736064).get_channel(915405860266704906)
-            await priv.send(f'{ctx.author.display_name} (uID {ctx.author.id} has this error:\n\n{error}')
+            msg.add_field(name="Error Type:", value=f"{error}", inline=False)
+            msg.add_field(name="Content of message:", value=ctx.message.content, inline=False)
             print(error)
+
+        await priv.send(embed=msg)
 
 
 def setup(client):
