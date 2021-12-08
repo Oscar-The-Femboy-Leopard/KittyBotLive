8668f9e63e553c7d8bb024faf27573de06b6f362
Oscar-The-Femboy-Leopard <74582007+Oscar-The-Femboy-Leopard@users.noreply.github.com>
2021-12-08 22:16:10 +0000

    Hug fix, putting it back to 1 person whilst Greedy is waiting to be used

--- Wynter_Integration/hug.py
+++ Wynter_Integration/hug.py
@@ -4,9 +4,9 @@ import json
 import discord
 import random
 import datetime
+
 from discord.ext import commands
 from config import random_color
-
 
 aliases = ['huggles', 'Hugs', 'Huggles']
 description = "This command uses Wynter's API, made by Darkmane Arweinydd#0069, to hug people!"
@@ -17,9 +17,9 @@ class Wholesome(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(aliases=aliases,
-                      description=description)
-    async def hug(self, ctx):
+    @commands.command(aliases=aliases, description=description)
+    # async def hug(self, ctx, m: commands.Greedy[discord.Member]):
+    async def hug(self, ctx, *, m: discord.Member):
 
         conn = http.client.HTTPSConnection("api.furrycentr.al")
         payload = ''
@@ -35,17 +35,10 @@ class Wholesome(commands.Cog):
         color = random.choice(random_color)
         guild = ctx.guild
         msg = ctx
-        # _ment_hug = hugged.mentions
-        # _ment_hug = hugged.mention
-
-        # blep = ctx.message.content.split(' ')
-        # hugged = blep[1]
-        # huggled = blep[2]
-        hugged = discord.Member
-        # huggled = discord.Member
-        _ment_hug = hugged.display_name
+        # hugged = discord.Member
+        _ment_hug = m.display_name
+        # _ment_hug = ", ".join(m)
         auth = msg.author.display_name
-        # ment_hug = huggled.display_name
 
         '''if hugged and huggled:
             hug = [
@@ -55,11 +48,11 @@ class Wholesome(commands.Cog):
 
         # else:
         hug = [
-                f"***{auth}** hugged **{_ment_hug}***",
-                   f"***{auth}** sneaks up on **{_ment_hug}** before giving them bear hugs*",
-                   f"*When **{_ment_hug}** isn't looking, **{auth}** just hugs them tightly*",
-                   f"***{auth}** hugs **{_ment_hug}** from behind*"
-            ]
+            f"***{auth}** hugged **{_ment_hug}***",
+            f"***{auth}** sneaks up on **{_ment_hug}** before giving them bear hugs*",
+            f"*When **{_ment_hug}** isn't looking, **{auth}** just hugs them tightly*",
+            f"***{auth}** hugs **{_ment_hug}** from behind*"
+        ]
         hugmessage = random.choice(hug)
 
         e_url = discord.Embed(color=color, timestamp=datetime.datetime.utcnow())
@@ -68,13 +61,6 @@ class Wholesome(commands.Cog):
         e_url.set_footer(text=f"{guild.name}", icon_url=guild.icon_url)
 
         if auth == _ment_hug:
-            '''e_url = discord.Embed(color=color, timestamp=_timestamp)
-            # e_url.set_author(name=self.client.user.name, icon_url=self.client.user.avatar_url)
-            # e_url.add_field(name=f"***{msg.author}** hugged **{_ment_hug}***", value=_blnk_value, inline=True)
-            e_url.add_field(name=hugmessage, value=f"[Link to image]({url})", inline=False)
-            e_url.set_image(url=url)
-            e_url.set_footer(text=f"{guild.name}", icon_url=guild.icon_url)
-            return await msg.channel.send(embed=e_url)'''
             return await msg.channel.send(f"You cannot hug yourself, {auth}, so I'll hug you! *hugs tightly*")
         else:
             return await msg.channel.send(embed=e_url)
